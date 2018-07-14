41d828afd74027f2f65eb887a0a91253029d0473
Veld <velddev@gmail.com>
2018-07-14 04:31:20 +0200

    add: gateway manager

--- gateway-manager.js
+++ gateway-manager.js
@@ -1,14 +1,30 @@
-const config = require('./config');
-const { spawn } = require('child_process');
+const config = require('./manager.config');
+const childProcess = require('child_process');
+
+function spawnGateway(index)
+{
+    let child = childProcess.spawn("node", ['gateway'], {
+        env: {
+            SHARD: index,
+            SHARDCOUNT: config.shardCount
+        }
+    })    
+
+    child.stdout.on('data', (data) => {
+        process.stdout.write(`[SHARD_${index}]: ${data}`);
+    });
+    
+    child.stderr.on('data', (data) => {
+        process.stderr.write(`[SHARD_${index}]: ${data}`);
+    });
+
+    child.on('exit', (num, signal) => {
+        console.log(`[SHARD_${index}] has closed unexpectingly, reconnecting...`);
+        spawnGateway(index);
+    });
+}
 
 for(var i = config.shardIndex; i < config.shardIndex + config.shardInit; i++)
 {
-    // let child = spawn('node',    
-    // child.stdout.on('data', (data) => {
-    //     console.log(`child stdout:\n${data}`);
-    // });
-    
-    // child.stderr.on('data', (data) => {
-    // console.error(`child stderr:\n${data}`);
-    // });
+    spawnGateway(i);
 }
